ebfef1eaef9b85ff62ecf6f86596ccea7818f92d
egi <daegi01@gmail.com>
2020-11-27 23:54:10 +0100

    converted to 1d again with pixelbuffer and all for better perf.

--- src/RenderGridVisualizer/RenderGridVisualizer.cs
+++ src/RenderGridVisualizer/RenderGridVisualizer.cs
@@ -1,28 +1,32 @@
-﻿using ColossalFramework.Math;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace RenderGridVisualizer
 {
     public class RenderGridVisualizer : MonoBehaviour
     {
-        private RenderGroup[] _renderGroups;
-        private FastList<RenderGroup> _renderedGroups;
-
         private readonly Color32 _accentColor = new Color32(255, 255, 255, 200);
         private readonly Color32 _backgroundColor = new Color32(255, 0, 0, 200);
         private readonly int _renderGridLength = 45;
-        private Texture2D _targetTexture;
 
-        private bool _isOn;
+        private RenderGroup[] _renderGroups;
+        private FastList<RenderGroup> _renderedGroups;
+
+        private int _renderGridMaxIndex;
+        private Texture2D _targetTexture;
+        private Color32[] _targetTextureBuffer;
         private OverlayGridTool _tool;
+        
+        private bool _isOn;
 
         private void Start()
         {
             _renderGroups = RenderManager.instance.m_groups;
             _renderedGroups = RenderManager.instance.m_renderedGroups;
 
+            _renderGridMaxIndex = _renderGridLength * _renderGridLength;
             _targetTexture = new Texture2D(_renderGridLength, _renderGridLength, TextureFormat.ARGB32, true, true);
             _targetTexture.filterMode = FilterMode.Point;
+            _targetTextureBuffer = new Color32[_renderGridLength * _renderGridLength];
 
             GameObject controllerGameObject = ToolsModifierControl.toolController.gameObject;
             _tool = controllerGameObject.GetComponent<OverlayGridTool>() ?? controllerGameObject.AddComponent<OverlayGridTool>();
@@ -41,28 +45,23 @@ namespace RenderGridVisualizer
 
             var renderedGroupIndex = 0;
             var renderedGroup = _renderedGroups[renderedGroupIndex];
-            for (int x = 0; x < _renderGridLength; x++)
+            for (int i = 0; i < _renderGridMaxIndex; i++)
             {
-                for (int y = 0; y < _renderGridLength; y++)
+                var renderGroup = _renderGroups[i];
+                if (renderGroup == renderedGroup)
                 {
-                    var renderGroupIndex = x * _renderGridLength + y;
-                    var renderGroup = _renderGroups[renderGroupIndex];
-                    if (renderedGroupIndex < _renderedGroups.m_size && renderGroup == renderedGroup)
-                    {
-                        _targetTexture.SetPixel(x, y, _accentColor);
+                    _targetTextureBuffer[i] = _accentColor;
 
-                        renderedGroupIndex++;
-                        renderedGroup = _renderedGroups[renderedGroupIndex];
-                    }
-                    else
-                    {
-                        _targetTexture.SetPixel(x, y, _backgroundColor);
-                    }
+                    renderedGroupIndex++;
+                    renderedGroup = _renderedGroups[renderedGroupIndex];
+                }
+                else
+                {
+                    _targetTextureBuffer[i] = _backgroundColor;
                 }
             }
 
-            //TODO convert to 1D again, and use SetPixels instead, should be faster.
-
+            _targetTexture.SetPixels32(_targetTextureBuffer);
             _targetTexture.Apply();
             GUI.DrawTexture(new Rect(new Vector2(0, 0), new Vector2(_renderGridLength * 8, _renderGridLength * 8)), _targetTexture);
 
